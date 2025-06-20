5b6b041fc66d6ef24e40ba8aaa3f5a4821a6239c
SNXWXH <snxwxh@gmail.com>
2025-06-20 13:30:25 +0900

    feat: StatisticsCard 컴포넌트 생성 및 타입 정의

---
+++ src/components/StatisticsCard.tsx
@@ -0,0 +1,42 @@
+import { IconKeyType, IconObjType } from '@/app/types/mypage'
+import { Award, BookOpen, FileText, TrendingUp } from 'lucide-react'
+
+export default function StatisticsCard({
+  title,
+}: {
+  title: IconKeyType
+}) {
+  const iconObj: IconObjType = {
+    totalBooks: {
+      title: '총 읽은 책',
+      component: <BookOpen className="h-8 w-8 text-blue-500" />,
+    },
+    reviewBooks: {
+      title: '작성한 독후감',
+      component: <FileText className="h-8 w-8 text-green-500" />,
+    },
+    currentStreak: {
+      title: '현재 연속 독서',
+      component: <TrendingUp className="h-8 w-8 text-orange-500" />,
+    },
+    longestStreak: {
+      title: '최장 연속 독서',
+      component: <Award className="h-8 w-8 text-purple-500" />,
+    },
+  }
+
+  return (
+    <div className="flex justify-between items-center w-72 h-28 bg-white border-1 border-gray-300 rounded-xl px-6 py-8">
+      <div>
+        <p className="text-sm text-gray-600 mb-1 text-">
+          {iconObj[title].title}
+        </p>
+        <span className="font-bold text-3xl">24</span>
+        {(title === 'currentStreak' || title === 'longestStreak') && (
+          <span className="font-bold text-3xl">일</span>
+        )}
+      </div>
+      {iconObj[title].component}
+    </div>
+  )
+}
