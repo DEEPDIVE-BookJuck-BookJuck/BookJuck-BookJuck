fca621142015a39954a017892cda95533409a7c9
SNXWXH <snxwxh@gmail.com>
2025-06-20 13:20:37 +0900

    feat: /mypage 라우팅 설정

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,14 @@
+export default function MyPage() {
+  return (
+    <>
+      <div className="flex flex-col gap-6">
+        <div>
+          <h1 className="text-3xl font-bold">마이페이지</h1>
+          <p className="mt-3 text-gray-600">
+            나의 독서 통계와 기록을 확인해보세요
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
