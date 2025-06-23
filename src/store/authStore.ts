e6d22d34151eba85caa25a31b526c9cac2d4d2c7
syrnjs <ksy21019@gmail.com>
2025-06-23 14:15:48 +0900

    refactor(types): 공통 auth 관련 타입 전환 및 분리

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,19 +1,7 @@
+import { AuthStateType } from '@/app/types/auth'
 import { create } from 'zustand'
 
-type User = {
-  id?: string // 로그인 시 반환
-  email: string // 공통 필드
-  nickname?: string // 회원가입 시 반환
-}
-
-type AuthState = {
-  accessToken: string | null
-  user: User | null
-  setAuth: (accessToken: string, user: User) => void
-  clearAuth: () => void
-}
-
-export const useAuthStore = create<AuthState>((set) => ({
+export const useAuthStore = create<AuthStateType>((set) => ({
   accessToken: null,
   user: null,
 
