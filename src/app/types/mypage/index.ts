34a4ca72026b231655f548f90999e01d52f4c6a0
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-06-23 14:02:19 +0900

    feat: mypage 퍼블리싱 및 폰트 / PR, ISSUE 템플릿 적용 (#9)
    * chore: Wanted Sans 폰트 적용
    
    * chore: PR 및 Issue Template 추가
    
    * feat: RecentBookItem 컴포넌트 props 타입 정의 및 interface로 변경
    
    * feat: RecentBook, RecentBookItem 컴포넌트 생성 및 적용

--- src/app/types/mypage/index.ts
+++ src/app/types/mypage/index.ts
@@ -6,10 +6,28 @@ export type IconKeyType =
   | 'currentStreak'
   | 'longestStreak'
 
-export type IconObjType = Record<
-  IconKeyType,
-  {
+export interface IconObjType {
+  totalBooks: {
     title: string
     component: ReactElement
   }
->
+  reviewBooks: {
+    title: string
+    component: ReactElement
+  }
+  currentStreak: {
+    title: string
+    component: ReactElement
+  }
+  longestStreak: {
+    title: string
+    component: ReactElement
+  }
+}
+
+export interface RecentBookItemPropsType {
+  title: string
+  author: string
+  date: string
+  rating: number
+}
