01fcd6d1afd5c18a59b178398b234ab5041ac5b4
구름 <dong21co@naver.com>
2025-06-24 10:35:51 +0900

    fix:타입오류 수정

---
+++ src/app/types/mypageHome/bookTypes.ts
@@ -0,0 +1,13 @@
+export interface RawBookItemType {
+  itemId: string | number
+  cover?: string
+  title?: string
+  author?: string
+}
+
+export interface BookType {
+  id: string | number
+  cover: string
+  title: string
+  author: string
+}
