80e5ef09bf239b7cf0da9d5bc261fd3de5482827
syrnjs <ksy21019@gmail.com>
2025-06-25 17:07:30 +0900

    style: 공용 레이아웃에 아이콘 추가 및 버튼 커서 포인터 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,26 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { BookOpen, House, Library, User } from 'lucide-react'
 
 export default function Header() {
   const pathname = usePathname()
 
   const navItems = [
-    { href: '/', label: '홈', iconClass: 'fas fa-house' },
+    {
+      href: '/',
+      label: '홈',
+      icon: () => <House className="size-4" />,
+    },
     {
       href: '/mylibrary',
       label: 'My Library',
-      iconClass: 'fas fa-book',
+      icon: () => <Library className="size-4" />,
     },
     {
       href: '/mypage',
       label: '마이페이지',
-      iconClass: 'fas fa-user',
+      icon: () => <User className="size-4" />,
     },
   ]
 
@@ -25,13 +30,13 @@ export default function Header() {
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3">
         {/* 로고 */}
         <div className="flex items-center gap-2 text-xl font-bold">
-          <i className="fa-solid fa-book-open text-blue-500"></i>
+          <BookOpen className="size-7 text-blue-500" />
           <Link href="/">BookHub</Link>
         </div>
 
         {/* 네비게이션 */}
         <nav className="flex-1 flex justify-center gap-6 text-sm font-medium">
-          {navItems.map(({ href, label, iconClass }) => {
+          {navItems.map(({ href, label, icon: Icon }) => {
             const isActive = pathname === href
 
             return (
@@ -46,7 +51,7 @@ export default function Header() {
                 }
               `}
               >
-                <i className={iconClass}></i>
+                <Icon />
                 {label}
               </Link>
             )
@@ -55,12 +60,12 @@ export default function Header() {
 
         <div className="flex gap-2 text-sm">
           <Link href="/auth/login">
-            <button className="px-4 py-2 border border-gray-200 rounded hover:bg-gray-100">
+            <button className="px-4 py-2 border border-gray-200 rounded hover:bg-gray-100 hover: cursor-pointer">
               로그인
             </button>
           </Link>
           <Link href="/auth/signup">
-            <button className="px-4 py-2 bg-slate-950 text-white rounded hover:bg-gray-800">
+            <button className="px-4 py-2 bg-slate-950 text-white rounded hover:bg-gray-800 hover: cursor-pointer">
               회원가입
             </button>
           </Link>
@@ -69,4 +74,3 @@ export default function Header() {
     </header>
   )
 }
-
