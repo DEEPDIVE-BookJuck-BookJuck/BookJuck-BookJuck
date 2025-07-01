603eda7b2ad85e26b47e8667fea2d8dbb050198e
SNXWXH <snxwxh@gmail.com>
2025-07-01 14:18:00 +0900

    feat: 프로필 정보/수정 API 연동 및 렌더링

--- src/app/(with-header)/(protected)/profile/_components/profile-item.tsx
+++ src/app/(with-header)/(protected)/profile/_components/profile-item.tsx
@@ -1,10 +1,68 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import Button from './button'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { fetchWithAuth } from '@/lib/fetch-with-auth'
+import { ProfileType } from '../_types'
 
 export default function ProfileItem() {
+  const [nickName, setNickName] = useState('')
+  const [email, setEmail] = useState('')
+  const [updateNickName, setUpdateNickName] = useState('')
+  const [active, setActive] = useState(false)
   const router = useRouter()
+
+  const clickHandler = () => {
+    const fetchNickName = async () => {
+      try {
+        const newNickName: ProfileType = await fetchWithAuth(
+          '/api/user/profile',
+          {
+            auth: true,
+            method: 'PATCH',
+            body: JSON.stringify({ nickName: updateNickName.trim() }),
+          },
+        )
+
+        setNickName(newNickName?.nickName)
+        setUpdateNickName('')
+        setActive(false)
+      } catch (error) {
+        if (error instanceof Error)
+          console.error('닉네임 변경 로딩 실패:', error.message)
+        else console.error('왜 나는지 모르는 에러:', error)
+      }
+    }
+    fetchNickName()
+  }
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value
+    setUpdateNickName(newValue)
+    setActive(newValue.trim() !== '')
+  }
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const profileData: ProfileType = await fetchWithAuth(
+          '/api/user/profile',
+          {
+            auth: true,
+            method: 'GET',
+          },
+        )
+
+        setNickName(profileData.nickName)
+        setEmail(profileData.email)
+      } catch (error) {
+        if (error instanceof Error)
+          console.error('프로필 데이터 로딩 실패:', error.message)
+        else console.error('왜 나는지 모르는 에러:', error)
+      }
+    }
+    fetchProfile()
+  }, [nickName])
 
   return (
     <div className="flex flex-col justify-center items-center gap-4">
@@ -13,7 +71,7 @@ export default function ProfileItem() {
         <label className="font-bold w-1/4 text-sm text-center pr-2 pt-1">
           E-mail
         </label>
-        <p className="w-2/3">test1@email.com</p>
+        <p className="w-2/3">{email}</p>
       </div>
       <div className="flex w-5/6 justify-between items-center">
         <label className="font-bold w-1/4 text-sm text-center pr-2 pt-1">
@@ -22,16 +80,30 @@ export default function ProfileItem() {
         <input
           type="text"
           name="nickName"
-          placeholder="삼룡파 곽두칠"
+          value={updateNickName}
+          onChange={handleInput}
+          placeholder={nickName}
           className="w-2/3 h-11 p-2 border border-zinc-200 rounded focus:ring-1 focus:ring-inset focus:ring-gray-400 focus:outline-none"
         />
       </div>
       <section className="flex w-1/2 justify-between mt-2">
-        <Button
-          text="취소"
-          handler={() => router.back()}
-        />
-        <Button text="수정" />
+        <button
+          className="px-4 py-2 border rounded border-gray-200 hover:bg-gray-100 cursor-pointer"
+          onClick={() => router.back()}
+        >
+          취소
+        </button>
+        <button
+          disabled={!active}
+          onClick={clickHandler}
+          className={`px-4 py-2 border rounded border-gray-200 ${
+            active
+              ? 'bg-slate-950 text-white hover:bg-gray-800 cursor-pointer'
+              : 'cursor-not-allowed'
+          }`}
+        >
+          수정
+        </button>
       </section>
     </div>
   )
