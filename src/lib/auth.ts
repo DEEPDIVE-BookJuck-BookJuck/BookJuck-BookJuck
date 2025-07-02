27db3434ea115f8635c586fc6b96fcf36295b2d5
권수영 <113653579+ksy21019@users.noreply.github.com>
2025-07-02 14:17:10 +0900

    feat: 환경 변수 수정 및 로그아웃 기능 구현 (#73)
    * refactor: 환경 변수 수정
    
    * feat: 로그아웃 기능 구현
    
    * style: 헤더 메뉴 텍스트 한글화

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,13 +5,13 @@ import {
   SignupResponseType,
 } from '@/app/(without-header)/auth/_types'
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export async function login({
   email,
   password,
 }: LoginParamsType): Promise<LoginResponseType> {
-  const res = await fetch(`${BASE_URL}/api/auth/signin`, {
+  const res = await fetch(`${API_URL}/api/auth/signin`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -32,7 +32,7 @@ export async function signup({
   email,
   password,
 }: SignupParamsType): Promise<SignupResponseType> {
-  const res = await fetch(`${BASE_URL}/api/auth/signup`, {
+  const res = await fetch(`${API_URL}/api/auth/signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -46,4 +46,14 @@ export async function signup({
   }
 
   return res.json()
+}
+
+export function logout() {
+  document.cookie = 'accessToken=; Max-Age=0; path=/'
+
+  if (typeof window !== 'undefined') {
+    const { clearAuth } =
+      require('@/store/auth-store').useAuthStore.getState()
+    clearAuth()
+  }
 }
