1af404648459053b87df1fadfd93f8d1d153723b
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-07-03 17:30:08 +0900

    refactor: 마이페이지 API 호출 방식 변경 및 타이틀 변경 (#91)
    * refactor: 순차 API 호출 Promise.allSettled로 변경
    
    * feat: 닉네임 기반 동적 타이틀로 변경
    
    * chore: 불필요한 주석 제거

--- src/app/(with-header)/(protected)/my-page/page.tsx
+++ src/app/(with-header)/(protected)/my-page/page.tsx
@@ -9,51 +9,66 @@ import {
   TagBookType,
 } from './_types'
 import { fetchWithAuthOnServer } from '@/lib/fetch-with-auth-server'
+import { ProfileType } from '../profile/_types'
 
 export default async function MyPage() {
   let statisicData: StatisicType | null = null
   let monthlyBookData: MonthlyBookType[] | null = null
   let tagBookData: TagBookType[] | null = null
   let recentBookData: RecentBookType[] | null = null
+  let profileData: ProfileType | null = null
 
   try {
-    statisicData = await fetchWithAuthOnServer(
-      '/api/reading/statistics',
-    )
-  } catch (error) {
-    if (error instanceof Error)
-      console.error('통계 데이터 로딩 실패:', error.message)
-    else console.error('왜 나는지 모르는 에러:', error)
-  }
+    const results = await Promise.allSettled<
+      [
+        Promise<StatisicType>,
+        Promise<MonthlyBookType[]>,
+        Promise<TagBookType[]>,
+        Promise<RecentBookType[]>,
+        Promise<ProfileType>,
+      ]
+    >([
+      fetchWithAuthOnServer('/api/reading/statistics'),
+      fetchWithAuthOnServer('/api/reading/monthly'),
+      fetchWithAuthOnServer('/api/reading/tags/statistics'),
+      fetchWithAuthOnServer('/api/library/review/recent'),
+      fetchWithAuthOnServer('/api/user/profile'),
+    ])
 
-  try {
-    monthlyBookData = await fetchWithAuthOnServer(
-      '/api/reading/monthly',
-    )
-  } catch (error) {
-    if (error instanceof Error)
-      console.error('월별 독서량 데이터 로딩 실패:', error.message)
-    else console.error('왜 나는지 모르는 에러:', error)
-  }
+    if (results[0].status === 'fulfilled')
+      statisicData = results[0].value
+    else console.error('통계 데이터 로딩 실패:', results[0].reason)
 
-  try {
-    tagBookData = await fetchWithAuthOnServer(
-      '/api/reading/tags/statistics',
-    )
-  } catch (error) {
-    if (error instanceof Error)
-      console.error('태그별 독서량 데이터 로딩 실패:', error.message)
-    else console.error('왜 나는지 모르는 에러:', error)
-  }
+    if (results[1].status === 'fulfilled')
+      monthlyBookData = results[1].value
+    else
+      console.error(
+        '월별 독서량 데이터 로딩 실패:',
+        results[1].reason,
+      )
 
-  try {
-    recentBookData = await fetchWithAuthOnServer(
-      '/api/library/review/recent',
-    )
+    if (results[2].status === 'fulfilled')
+      tagBookData = results[2].value
+    else
+      console.error(
+        '태그별 데이터 데이터 로딩 실패:',
+        results[2].reason,
+      )
+
+    if (results[3].status === 'fulfilled')
+      recentBookData = results[3].value
+    else
+      console.error(
+        '최근 읽은 책 데이터 로딩 실패:',
+        results[3].reason,
+      )
+
+    if (results[4].status === 'fulfilled')
+      profileData = results[4].value
+    else
+      console.error('프로파일 데이터 로딩 실패:', results[4].reason)
   } catch (error) {
-    if (error instanceof Error)
-      console.error('최근 읽은 책 데이터 로딩 실패:', error.message)
-    else console.error('왜 나는지 모르는 에러:', error)
+    console.error('전체 데이터 로딩 중 오류:', error)
   }
 
   return (
@@ -61,7 +76,9 @@ export default async function MyPage() {
       <div className="flex flex-col gap-6">
         <section className="flex items-center justify-between">
           <div>
-            <h1 className="text-3xl font-bold">마이페이지</h1>
+            <h1 className="text-3xl font-bold">
+              {profileData?.nickName}의 독서통계
+            </h1>
             <p className="mt-3 text-gray-600">
               나의 독서 통계와 기록을 확인해보세요
             </p>
