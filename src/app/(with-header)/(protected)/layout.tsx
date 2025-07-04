47695b627742860f28b16c93ce5031e5d136383f
권수영 <113653579+ksy21019@users.noreply.github.com>
2025-07-04 10:05:26 +0900

    feat: Protected Route 활성화 (#94)

--- src/app/(with-header)/(protected)/layout.tsx
+++ src/app/(with-header)/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode, useEffect, useState } from 'react'
-import { useRouter, usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { useAuthStore } from '@/store/auth-store'
 import Modal from '@/common/modal'
 
@@ -17,15 +17,8 @@ export default function ProtectedLayout({
   const [showModal, setShowModal] = useState(false)
   const [countdown, setCountdown] = useState(3) // 시작 초 설정 : 3초
 
-  // API 연동 완료 후 수정 예정
-  const pathname = usePathname()
-  const protectedPaths = ['/my-library', '/my-page', '/profile']
-  const isProtected = protectedPaths.some((path) =>
-    pathname.startsWith(path),
-  )
-
   useEffect(() => {
-    if (!isProtected && !user) {
+    if (!user) {
       setShowModal(true)
 
       const interval = setInterval(() => {
@@ -40,11 +33,11 @@ export default function ProtectedLayout({
 
       return () => clearInterval(interval)
     }
-  }, [user, router, isProtected])
+  }, [user, router])
 
-  if (!isProtected && !user && !showModal) return null
+  if (!user && !showModal) return null
 
-  if (!isProtected && !user && showModal) {
+  if (!user && showModal) {
     return (
       <Modal>
         <p className="text-center text-lg font-semibold mb-2">
