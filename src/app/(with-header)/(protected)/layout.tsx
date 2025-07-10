91064df5c94701a30aee5fbf30c110ecd9142e01
syrnjs <ksy21019@gmail.com>
2025-07-10 16:40:29 +0900

    feat: 비로그인 상태 접근 시 로그인 안내 및 리디렉션 로직 개선

--- src/app/(with-header)/(protected)/layout.tsx
+++ src/app/(with-header)/(protected)/layout.tsx
@@ -16,7 +16,6 @@ export default function ProtectedLayout({
   const pathname = usePathname()
   const { user } = useAuthStore((state) => state)
   const [showModal, setShowModal] = useState(false)
-  const [countdown, setCountdown] = useState(3) // 시작 초 설정 : 3초
 
   useEffect(() => {
     if (!user) {
@@ -26,33 +25,32 @@ export default function ProtectedLayout({
       }
 
       setShowModal(true)
-      setCountdown(3)
-
-      const timer = setInterval(() => {
-        setCountdown((prev) => prev - 1)
-      }, 1000)
-
-      return () => clearInterval(timer)
     }
   }, [user, pathname])
-
-  useEffect(() => {
-    if (countdown === 0 && !user) {
-      router.replace('/auth/log-in')
-    }
-  }, [countdown, user, router])
-
-  if (!user && !showModal) return null
 
   if (!user && showModal) {
     return (
       <Modal>
-        <p className="text-center text-lg font-semibold mb-2">
-          로그인이 필요합니다
-        </p>
-        <p className="text-center text-sm text-gray-600">
-          {countdown}초 후 로그인 페이지로 이동합니다.
-        </p>
+        <div className="mb-4">
+          <p className="whitespace-normal text-center text-md">
+            로그인이 필요한 서비스입니다.
+          </p>
+          <p className="text-center text-md">로그인 하시겠습니까?</p>
+        </div>
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={() => router.push('/auth/log-in')}
+            className="bg-slate-950 hover:bg-gray-800 hover:cursor-pointer text-white text-sm py-2 px-4 rounded"
+          >
+            예
+          </button>
+          <button
+            onClick={() => router.push('/')}
+            className="border border-gray-200 hover:bg-gray-100 hover:cursor-pointer text-sm py-2 px-4 rounded"
+          >
+            아니오
+          </button>
+        </div>
       </Modal>
     )
   }
