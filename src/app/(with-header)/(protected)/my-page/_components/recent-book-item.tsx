e5c62056845a6a6d54e4dcd6ae8406245c5911fe
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-07-11 10:06:50 +0900

    feat: my-page 최근 읽은 책 라우팅 설정 (#144)

--- src/app/(with-header)/(protected)/my-page/_components/recent-book-item.tsx
+++ src/app/(with-header)/(protected)/my-page/_components/recent-book-item.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import TagList from '../../_components/tag-list'
 
 interface RecentBookItemPropsType {
+  id: string
   title: string
   author: string
   date: string
@@ -9,6 +11,7 @@ interface RecentBookItemPropsType {
 }
 
 export default function RecentBookItem({
+  id,
   title,
   author,
   date = '',
@@ -16,24 +19,28 @@ export default function RecentBookItem({
   tags,
 }: RecentBookItemPropsType) {
   return (
-    <div className="flex justify-between items-center h-28 bg-blue-50/50 border-blue-50 rounded-lg px-2 md:px-6">
-      <div className="w-3/5">
-        <p className="font-bold truncate">{title}</p>
-        <p className="text-sm text-gray-600 truncate">{author}</p>
-        <p className="text-xs text-gray-400 mb-2">{date}</p>
-        <div className="overflow-hidden h-6">
-          <TagList
-            tags={tags}
-            style="text-gray-600 bg-slate-200/50"
-          />
+    <Link href={`/my-library/${id}/view`}>
+      <div className="flex justify-between items-center h-28 bg-blue-50/50 border-blue-50 rounded-lg px-2 md:px-6">
+        <div className="w-3/5">
+          <p className="font-bold truncate">{title}</p>
+          <p className="text-sm text-gray-600 truncate">{author}</p>
+          <p className="text-xs text-gray-400 mb-2">{date}</p>
+          <div className="overflow-hidden h-6">
+            <TagList
+              tags={tags}
+              style="text-gray-600 bg-slate-200/50"
+            />
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <span className="text-yellow-500">
+            {'★'.repeat(rating)}
+          </span>
+          <span className="text-gray-300">
+            {'★'.repeat(5 - rating)}
+          </span>
         </div>
       </div>
-      <div className="flex justify-end">
-        <span className="text-yellow-500">{'★'.repeat(rating)}</span>
-        <span className="text-gray-300">
-          {'★'.repeat(5 - rating)}
-        </span>
-      </div>
-    </div>
+    </Link>
   )
 }
