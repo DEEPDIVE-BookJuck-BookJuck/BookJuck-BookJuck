70bbcc36f68f0e55be0b58ef8902fc1995990dc7
조연경 <sig.yg5057@gmail.com>
2025-07-11 12:01:44 +0900

    style: my-library 독후감 상세페이지 스켈리톤 UI 수정

--- src/app/(with-header)/(protected)/my-library/_components/skeleton/detail-page-skeleton.tsx
+++ src/app/(with-header)/(protected)/my-library/_components/skeleton/detail-page-skeleton.tsx
@@ -1,13 +1,21 @@
 import BoxSkeleton from './box-skeleton'
 
-export default function DetailPageSkeleton() {
+interface DetailPageSkeletonProps {
+  mode?: 'view' | 'edit'
+}
+
+export default function DetailPageSkeleton({
+  mode = 'edit',
+}: DetailPageSkeletonProps) {
+  const boxHeight = mode === 'view' ? 'h-90' : 'h-[700px]'
+
   return (
     <>
       <div className="lg:col-span-1">
         <BoxSkeleton className="h-80 rounded-md" />
       </div>
       <div className="lg:col-span-2 space-y-6">
-        <BoxSkeleton className="h-140 w-full rounded-md" />
+        <BoxSkeleton className={`${boxHeight} w-full rounded-md`} />
       </div>
     </>
   )
